80441aec13eea7ed40783cf37a3e01987a95e39e
sshamoun <devoteam@1e:dd:ab:d8:14:6f.home>
2025-05-18 23:27:17 +0200

    added ci github workflow

--- src/app.py
+++ src/app.py
@@ -17,6 +17,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
         'hostname': socket.gethostname()
+        'message': "You are doing great!"
     })
 @app.route('/api/v1/healthz')
 # ‘/’ URL is bound with hello_world() function.
