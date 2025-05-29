a4c2d2219a672fdbd4d554fa9f34d1a8c82ffa14
sshamoun <devoteam@16:f0:1b:29:46:3e.home>
2025-05-29 16:42:48 +0200

    cicd updated added github token

--- src/app.py
+++ src/app.py
@@ -17,7 +17,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
         'hostname': socket.gethostname()
-        'message': "You are doing great! Wajwwwww!!!!"
+        'message': "You are doing great! Wajwwwww!!"
     })
 @app.route('/api/v1/healthz')
 # ‘/’ URL is bound with hello_world() function.
